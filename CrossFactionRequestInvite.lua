2033c37f375d70220ce7176dfa458d957879b70c
Mark W <numedain@gmail.com>
2024-06-01 15:48:22 +0200

    fix an error when opening the friends frame in combat

--- CrossFactionRequestInvite.lua
+++ CrossFactionRequestInvite.lua
@@ -34,6 +34,40 @@ local function onEnter(travelPassButton)
     end
 end
 
+local eventFrame = CreateFrame("Frame");
+eventFrame.combatLockdownQueue = {};
+eventFrame:SetScript("OnEvent", function(self, event, ...)
+    if self[event] then
+        self[event](self, ...);
+    end
+end);
+
+--- @param func function
+--- @param ... any # arguments
+local function addToCombatLockdownQueue(func, ...)
+    if #eventFrame.combatLockdownQueue == 0 then
+        eventFrame:RegisterEvent("PLAYER_REGEN_ENABLED");
+    end
+
+    tinsert(eventFrame.combatLockdownQueue, { func = func, args = { ... } });
+end
+
+function eventFrame:PLAYER_REGEN_ENABLED()
+    self:UnregisterEvent("PLAYER_REGEN_ENABLED");
+    if #self.combatLockdownQueue == 0 then return; end
+
+    for _, item in pairs(self.combatLockdownQueue) do
+        item.func(unpack(item.args));
+    end
+    wipe(self.combatLockdownQueue);
+end
+
+local function passThroughLeftClick(btn)
+    -- pass through the leftclick for default behaviour
+    -- note: in 11.0, we can use SetPropagateMouseMotion, and hook the travelPassButton's OnEnter
+    btn:SetPassThroughButtons('LeftButton');
+end
+
 local function handleFriendListButtons()
     if
         FriendsListFrame
@@ -49,12 +83,14 @@ local function handleFriendListButtons()
             button.CrossFactionRequestInviteButton = CreateFrame('BUTTON', nil, button.travelPassButton);
             button.CrossFactionRequestInviteButton:SetAllPoints();
             button.CrossFactionRequestInviteButton:RegisterForClicks('RightButtonDown');
-            -- pass through the leftclick for default behaviour
-            -- note: in 11.0, we can use SetPropagateMouseMotion, and hook the travelPassButton's OnEnter
-            button.CrossFactionRequestInviteButton:SetPassThroughButtons('LeftButton');
             button.CrossFactionRequestInviteButton:SetScript('OnEnter', function() onEnter(button.travelPassButton); end);
             button.CrossFactionRequestInviteButton:SetScript('OnLeave', function() GameTooltip:Hide(); end);
             button.CrossFactionRequestInviteButton:SetScript('OnClick', function() onClick(button.travelPassButton); end);
+            if not InCombatLockdown() then
+                passThroughLeftClick(button.CrossFactionRequestInviteButton);
+            else
+                addToCombatLockdownQueue(passThroughLeftClick, button.CrossFactionRequestInviteButton);
+            end
         end
     end
 end
